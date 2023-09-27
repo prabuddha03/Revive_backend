f062ea32201c2b27cd293fe05bb1c87fab43ce43
Prabuddha Chowdhury <prabuddha.chowdhury@gmail.com>
2023-09-27 09:29:21 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const tourController = require('./../controllers/tourController');
+const productController = require('./../controllers/productController');
 const authController = require('./../controllers/authController');
 const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
+
+router.use('/:tourId/reviews', reviewRouter);
+
+router
+  .route('/top-5-cheap')
+  .get(productController.aliasTopProducts, productController.getAllProducts);
+
+  
