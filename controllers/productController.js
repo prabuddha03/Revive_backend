c43e2920cfab158c40da0fc3501b5cacd0d25428
Prabuddha Chowdhury <prabuddha.chowdhury@gmail.com>
2023-09-27 06:54:16 +0530

    Update productController.js

---
+++ controllers/productController.js
@@ -0,0 +1,4 @@
+const Product = require('./../models/productModel');
+const catchAsync = require('./../utils/catchAsync');
+const factory = require('./handlerFactory');
+const AppError = require('./../utils/appError');
