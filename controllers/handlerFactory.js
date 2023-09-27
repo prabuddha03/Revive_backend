6a52264e5311362ca8c54f60322da20443adba81
Prabuddha Chowdhury <prabuddha.chowdhury@gmail.com>
2023-09-27 07:02:35 +0530

    Update handlerFactory.js

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -3,13 +3,31 @@ const AppError = require('./../utils/appError');
 const APIFeatures = require('./../utils/apiFeatures');
 
 exports.createOne = Model =>
-catchAsync(async(req, res, next) => {
+  catchAsync(async (req, res, next) => {
     const doc = await Model.create(req.body);
 
     res.status(201).json({
-        status: 'success',
-        data: {
-            data: doc
-        }
+      status: 'success',
+      data: {
+        data: doc
+      }
     });
-});
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findByID(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No Document with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  });
