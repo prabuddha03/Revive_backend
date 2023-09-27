32a838019957f55c15096ca50900fb7b08ed08b4
Prabuddha Chowdhury <prabuddha.chowdhury@gmail.com>
2023-09-27 07:16:01 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,3 +2,15 @@ const Product = require('./../models/productModel');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('./../utils/appError');
+
+exports.getAllProducts = factory.getAll(Product);
+exports.getProduct = factory.getOne(Product, { path: 'reviews' });
+exports.createProduct = factory.createOne(Product);
+exports.updateProduct = factory.updateOne(Product);
+exports.deleteProduct = factory.deleteOne(Product);
+
+exports.aliasTopProducts = (req, res, next) => {
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    
+}
